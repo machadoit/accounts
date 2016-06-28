098de3b742f295f7244ef888fed52a312d0e0f1f
Andrew Brett <git@bretts.org>
2016-06-28 10:07:02 +0100

    Add writing of new transactions to disk

--- src/main/scala/accounts/record/repository/file/FileRecordRepository.scala
+++ src/main/scala/accounts/record/repository/file/FileRecordRepository.scala
@@ -1,6 +1,5 @@
 package accounts.record.repository.file
 
-
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.time.temporal.{TemporalAccessor, TemporalQuery}
@@ -19,16 +18,16 @@ class FileRecordRepository(file: java.io.File) extends RecordRepository with Str
     override def queryFrom(temporal: TemporalAccessor): LocalDate = LocalDate.from(temporal)
   }
 
-  override def all: Seq[Record] = stored ++ saved
+  override def all: Seq[Record] = loaded ++ saved
 
-  private val stored: Seq[Record] = {
+  private val loaded: Seq[Record] = {
     val reader = CSVReader.open(file)
     try {
       for {
         row <- reader.all
       } yield {
         val record = parse(row)
-        logger.debug(s"Record: $record")
+        logger.trace(s"Loaded: $record")
         record
       }
     } finally {
@@ -39,6 +38,11 @@ class FileRecordRepository(file: java.io.File) extends RecordRepository with Str
   private val saved: mutable.Buffer[Record] = mutable.Buffer()
 
   override def save(r: Record): Unit = {
+    val writer = CSVWriter.open(file, append = true)
+    val row = format(r)
+    logger.debug(s"Storing: $row")
+    writer.writeRow(row)
+    writer.close()
     saved += r
   }
 
@@ -68,6 +72,33 @@ class FileRecordRepository(file: java.io.File) extends RecordRepository with Str
   } catch {
     case NonFatal(e) => throw new IllegalArgumentException(s"Failed to parse line: $fields", e)
   }
+
+  private def format(r: Record): Seq[Any] = r match {
+    case t: Transaction => Seq(
+      dateFormatter.format(t.date),
+      t.description,
+      TransactionType.toInt(t.transactionType),
+      format(t.debit),
+      format(t.credit),
+      t.incomeType.value,
+      t.reference,
+      t.accountType.value,
+      0 // for backward-compatibility
+    )
+    case b: OpeningBalance => Seq(
+      dateFormatter.format(b.date),
+      b.description,
+      0,
+      format(b.debit),
+      format(b.credit),
+      0,
+      1,
+      1,
+      0 // for backward-compatibility
+    )
+  }
+
+  private def format(bd: BigDecimal): String = f"$bd%.2f"
 
   private def accountType(value: String): AccountType =
     Some(value).filter(!_.isEmpty).flatMap { s =>
