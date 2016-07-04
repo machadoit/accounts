bc3c84e786f777a60967a9cca1ceea97fbcc8565
Andrew Brett <git@bretts.org>
2016-07-04 22:30:49 +0100

    Suppress logging in tests

--- src/test/scala/accounts/view/ViewTest.scala
+++ src/test/scala/accounts/view/ViewTest.scala
@@ -1,7 +1,13 @@
 package accounts.view
 
+import com.typesafe.scalalogging.StrictLogging
 import io.scalatestfx.api.SfxRobot
 import io.scalatestfx.framework.scalatest.JFXAppFixture
 import org.scalatest.WordSpec
 
-trait ViewTest extends WordSpec with SfxRobot with JFXAppFixture with ViewMatchers
+trait ViewTest extends WordSpec with SfxRobot with JFXAppFixture with ViewMatchers with StrictLogging {
+
+  Thread.setDefaultUncaughtExceptionHandler { (t, e) =>
+    logger.error(s"Uncaught exception in thread '$t':", e)
+  }
+}
